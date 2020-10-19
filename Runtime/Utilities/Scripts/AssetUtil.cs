87ed423588a7417752a3bd8f21852f8d6c72c60c
Carsten Gedrat <kukkitime@gmail.com>
2020-10-19 19:47:25 +0200

    exclude assteUtil in builds

--- Runtime/Utilities/Scripts/AssetUtil.cs
+++ Runtime/Utilities/Scripts/AssetUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using System.Linq;
 using Game.Base.Utilities;
 using Game.Settings;
@@ -127,3 +128,4 @@ namespace Game.Base
         }
     }
 }
+#endif
