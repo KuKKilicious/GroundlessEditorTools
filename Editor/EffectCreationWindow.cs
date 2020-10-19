2abf18d421a2a4f72296abaac3b572e5a2804a96
Carsten Gedrat <kukkitime@gmail.com>
2020-10-19 19:49:39 +0200

    exclude creation Windows from builds

--- Editor/EffectCreationWindow.cs
+++ Editor/EffectCreationWindow.cs
@@ -1,4 +1,5 @@
-﻿using Game.Base;
+﻿#if UNITY_EDITOR
+using Game.Base;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
 using UnityEditor;
@@ -103,3 +104,4 @@ namespace Game.Editor
 }
 
 
+#endif
