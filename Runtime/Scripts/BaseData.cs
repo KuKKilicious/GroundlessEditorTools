a04f885e3bfb280d4fa27e2126487eda20663d96
KuKKi <clouseyda@yahoo.de>
2021-05-17 13:36:11 +0200

    Add Neon Quants to StatTypes

--- Runtime/Scripts/BaseData.cs
+++ Runtime/Scripts/BaseData.cs
@@ -30,7 +30,7 @@ namespace Game.Base
 
     public enum StatType
     {
-        MaxHP, CurrentHP, MaxShield, Shield, AttackSpeed, AttackDamage, CritDamage, CritPercentage, GoldGain, SplashDamage, RarityChance, AbilityAmp, LifeLeech
+        MaxHP, CurrentHP, MaxShield, Shield, AttackSpeed, AttackDamage, CritDamage, CritPercentage, GoldGain, SplashDamage, RarityChance, AbilityAmp, LifeLeech, NeonQuant, MaxNeonQuant
     }
 
     public enum EffectExplanation
